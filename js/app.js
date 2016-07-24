7ba9e8c876960205cf3fe38d41fd8f8229422a8b
Juan Bruno MacBook <juan.marcos.bruno@gmail.com>
2016-07-24 21:34:11 +0100

    Initial Project commit: Includes project structure and basic files

---
+++ js/app.js
@@ -0,0 +1,2 @@
+//Main module
+var app = angular.module('myApp', []);
---
+++ js/controller/MainController.js
@@ -0,0 +1,5 @@
+//Main Controller
+app.controller('MainController', ['$scope', function($scope) {
+    $scope.title = 'Top Sellers in E-Books';
+    $scope.promo = 'Promotions On the rise!';
+}]);
