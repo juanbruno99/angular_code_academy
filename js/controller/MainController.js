3c1b59b0afbfdde763cbcc7217521f343e9474c4
Juan Bruno MacBook <juan.marcos.bruno@gmail.com>
2016-07-28 20:54:05 +0100

    Addition of a collection with products as the model in controller's scope, also function of scope in controller as well

--- js/controller/MainController.js
+++ js/controller/MainController.js
@@ -2,9 +2,24 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.title = 'Top Sellers in E-Books';
     $scope.promo = 'Promotions On the rise!';
-    $scope.product = {
-        name:'The Book of Trees',
-        price:19,
-        pubdate: new Date('2013','03','08')
+    
+    $scope.products = [ 
+          { 
+            name: 'The Book of Trees', 
+            price: 19, 
+            pubdate: new Date('2014', '03', '08'), 
+            cover: 'img/the-book-of-trees.jpg',
+            likes: 0 
+          }, 
+          { 
+            name: 'Program or be Programmed', 
+            price: 8, 
+            pubdate: new Date('2013', '08', '01'), 
+            cover: 'img/program-or-be-programmed.jpg',
+            likes: 0 
+          } 
+        ];
+    $scope.plusOne = function(index) {
+        $scope.products[index].likes+=1;
     };
 }]);
